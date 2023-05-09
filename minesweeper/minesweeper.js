c842b30ab2401b75ac48794e43452b10917e9cf6
Noah Synowiec <noahsyn1@gmail.com>
2023-05-09 18:27:17 -0400

    add flag toggle for mobile

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -7,9 +7,9 @@ const ctx = canvas.getContext("2d");
 // size canvas based on orientation
 if (window.innerWidth < window.innerHeight) {
     canvas.width = window.innerWidth;
-    canvas.height = window.innerWidth;
+    canvas.height = window.innerHeight*4/5;
 } else {
-    canvas.width = window.innerHeight*4/5;
+    canvas.width = window.innerWidth;
     canvas.height = window.innerHeight*4/5;
 }
 let width = canvas.width;
@@ -21,7 +21,20 @@ let grid_size = parseInt(document.getElementById("grid_size").value);
 let mine_percentage = parseInt(document.getElementById("mine_percentage").value);
 let grid_width = grid_size;
 let grid_height = grid_width;
-let cell_size = canvas.height / grid_height;
+if (grid_width < grid_height) {
+    var cell_size = canvas.height / grid_height;
+} else {
+    var cell_size = canvas.width / grid_width;
+}
+var cell_size = Math.min(canvas.height / grid_height, canvas.width / grid_width)
+var canvas_left_offset = (canvas.width - (cell_size*grid_width))/2
+var canvas_top_offset = (canvas.height - (cell_size*grid_height))/2
+var reverseClick = false;
+
+console.log(canvas.width, canvas.height)
+console.log(cell_size*grid_width, cell_size*grid_height)
+console.log(canvas_left_offset, canvas_top_offset)
+
 let percent_fill = mine_percentage/100;
 let grid = []
 
@@ -39,8 +52,8 @@ class Cell {
     // get the colors of cell based on its attributes
     getColors() {
         // set colors based on position
-        const row_mod = (Math.floor(155 / grid_width)) * this.row + 50
-        const col_mod = (Math.floor(155 / grid_height)) * this.col + 50
+        const row_mod = (Math.floor(155 / grid_width)) * this.row + 75
+        const col_mod = (Math.floor(155 / grid_height)) * this.col + 75
         const r = row_mod
         const g = Math.floor((row_mod + col_mod) / 4)
         const b = col_mod
@@ -107,39 +120,38 @@ class Cell {
 
         // if not clicked, show 3d cell
         if (!this.clicked) {
-            drawShape(vertices["outer_tri_NW"], colors["lighter"]); 
-            drawShape(vertices["outer_tri_SE"], colors["darker"]); 
-            drawShape(vertices["inner_sq"], colors["middle"]); 
+            drawShapeWithOffset(vertices["outer_tri_NW"], colors["lighter"]); 
+            drawShapeWithOffset(vertices["outer_tri_SE"], colors["darker"]); 
+            drawShapeWithOffset(vertices["inner_sq"], colors["middle"]); 
 
         // if clicked, show flat cell
         } else {
-            drawShape(vertices["outer_sq"], colors["darker"]); 
-            drawShape(vertices["bordered_sq"], colors["middle"]); 
+            drawShapeWithOffset(vertices["outer_sq"], colors["darker"]); 
+            drawShapeWithOffset(vertices["bordered_sq"], colors["middle"]); 
         }
 
         // if clicked and cell has num > 0, show num
         if (this.clicked && this.cell_num > 0) {
-            ctx.font = `${cell_size/(5/3)}px verdana`;
-            ctx.fillStyle = "white";
-            ctx.fillText(this.cell_num, this.row*cell_size + cell_size / (3.3), this.col*cell_size + cell_size / (1.33));
+            let x = this.row*cell_size + cell_size / (3.3)
+            let y = this.col*cell_size + cell_size / (1.33)
+
+            drawTextWithOffset(this.cell_num, x, y, `${cell_size/(5/3)}px verdana`, "white", "fill");
         }
 
         // if flagged, show flag
         if (this.flagged) {
-            let x = this.row*cell_size + cell_size / (4);
-            let y = this.col*cell_size + cell_size / (1.5);
-            ctx.font = `${cell_size/(2.5)}px verdana`;
-            ctx.fillStyle = "white";
-            ctx.fillText("🚩", x,  y);
+            let x = this.row*cell_size + cell_size / (4)
+            let y = this.col*cell_size + cell_size / (1.5)
+
+            drawTextWithOffset("🚩", x, y, `${cell_size/(2.5)}px verdana`, "white", "fill");
         }      
         
         // if mine, show mine
         if (this.is_mine && this.clicked) {
-            let x = this.row*cell_size + cell_size / (3.3);
-            let y = this.col*cell_size + cell_size / (1.5);
-            ctx.font = `${cell_size/(2.25)}px verdana`;
-            ctx.fillStyle = "white";
-            ctx.strokeText("☼", x,  y);
+            let x = this.row*cell_size + cell_size / (3.3)
+            let y = this.col*cell_size + cell_size / (1.5)
+
+            drawTextWithOffset("☼", x, y, `${cell_size/(2.25)}px verdana`, "black", "stroke");
         }        
     }
 };
@@ -161,9 +173,9 @@ function refresh() {
     // size canvas based on orientation
     if (window.innerWidth < window.innerHeight) {
         canvas.width = window.innerWidth;
-        canvas.height = window.innerWidth;
+        canvas.height = window.innerHeight*4/5;
     } else {
-        canvas.width = window.innerHeight*4/5;
+        canvas.width = window.innerWidth;
         canvas.height = window.innerHeight*4/5;
     }
     width = canvas.width;
@@ -175,7 +187,11 @@ function refresh() {
     mine_percentage = parseInt(document.getElementById("mine_percentage").value);
     grid_width = grid_size;
     grid_height = grid_width;
-    cell_size = canvas.height / grid_height;
+
+    var cell_size = Math.min(canvas.height / grid_height, canvas.width / grid_width)
+    var canvas_left_offset = canvas.width - (cell_size*grid_width)/2
+    var canvas_top_offset = canvas.height - (cell_size*grid_height)
+
     percent_fill = mine_percentage/100;
     grid = []
 
@@ -204,7 +220,7 @@ function generateGrid() {
                 for (let i = -1; i <= 1; i++) {     // loop through cells around curr_cell
                     for (let j = -1; j <=1; j++) {
                                                     // if within range and cell is mine, increment curr_cell num
-                        if (0 <= row+i && row+i < grid_height && 0 <= col+j && col+j < grid_width) {
+                        if (0 <= row+i && row+i < grid_width && 0 <= col+j && col+j < grid_height) {
                             if (grid[row + i][col + j].is_mine) {
                                 curr_cell.cell_num++;      
                             }
@@ -216,11 +232,32 @@ function generateGrid() {
     }
 }
 
+// get cell from grid using offsets
+function getCellCoords(x, y) {
+    row = Math.floor((x-canvas_left_offset) / cell_size);
+    col = Math.floor((y-canvas_top_offset) / cell_size);
+
+    // detect coords out of grid
+    if (0 > row || row >= grid_width || 0 > col || col >= grid_height) {
+        return -1;
+    }
+
+    const cell = grid[row][col]         // get cell from grid
+    return cell;
+}
+
+// swap right and left click functionality
+function swapClick() {
+    reverseClick = !reverseClick;
+}
+
 // 'click' on the cell corresponding to the mouse's x, y position
 function click(x, y) {
-    row = Math.floor(x / cell_size);
-    col = Math.floor(y / cell_size);
-    const cell = grid[row][col]         // get cell from grid
+    const cell = getCellCoords(x, y)
+
+    if (cell == -1) {   // if coords are out of grid, handle and return
+        swapClick();
+    }   
 
     if (cell.flagged || cell.clicked) { // return if clicked or flagged
         return;
@@ -236,9 +273,11 @@ function click(x, y) {
 
 // flag the cell corresponding to the mouse's x, y position
 function flag(x, y) {
-    row = Math.floor(x / cell_size);
-    col = Math.floor(y / cell_size);
-    const cell = grid[row][col]         // get cell from grid
+    const cell = getCellCoords(x, y);
+
+    if (cell == -1) {   // if coords are out of grid, handle and return
+        swapClick();
+    }   
 
     if (cell.clicked) {                 // ignore if clicked
         return;
@@ -281,25 +320,59 @@ function draw() {
             cell.drawSelf();                            // draw cell
         }
     }
+
+    if (canvas.width<canvas.height) {
+        var x = (canvas.width/2.15);
+        var y = (canvas_top_offset/3) * -1;
+    } else {
+        var x = (grid_width*cell_size) + (canvas_left_offset/3);
+        var y = (canvas.height/1.9);
+    }
+    
+    // draw click type symbol
+    if (reverseClick) {
+        drawTextWithOffset("👉", x, y, `${cell_size/1.5}px verdana`, "white", "fill")
+    } else {
+        drawTextWithOffset("🚩", x, y, `${cell_size/1.5}px verdana`, "white", "fill")
+    }
 }
 
 // draw a shape using the given vertices and color
-function drawShape(vertexList, color) {
+// offset using calculated left and top canvas offset
+function drawShapeWithOffset(vertexList, color) {
     ctx.beginPath();
 
-    ctx.moveTo(vertexList[0][0], vertexList[0][1])
+    ctx.moveTo(vertexList[0][0] + canvas_left_offset, vertexList[0][1] + canvas_top_offset)
     for (let i = 1; i < vertexList.length; i++) {
-        ctx.lineTo(vertexList[i][0], vertexList[i][1]);
+        ctx.lineTo(vertexList[i][0] + canvas_left_offset, vertexList[i][1] + canvas_top_offset);
     }
     ctx.fillStyle = color;
     ctx.fill();
 }
 
+// draw text using the given font and fill/stroke
+// offset using calculated left and top canvas offset
+function drawTextWithOffset(text, x, y, font, color, style) {
+    x += canvas_left_offset;
+    y += canvas_top_offset;
+    ctx.font = font;
+    if (style == "fill") {
+        ctx.fillStyle = color;
+        ctx.fillText(text, x, y);
+    } else if (style == "stroke") {
+        ctx.strokeStyle = color;
+        ctx.strokeText(text, x, y);
+    }  
+}
+
 // handle the mouse click event
 function mouseUpHandler(e) {
     if (e.button == 0) {
-        click(e.offsetX, e.offsetY);
+        if (reverseClick) { flag(e.offsetX, e.offsetY); }
+        else              { click(e.offsetX, e.offsetY); }
+        
     } else if (e.button == 2) {
-        flag(e.offsetX, e.offsetY);
+        if (reverseClick) { click(e.offsetX, e.offsetY); }
+        else              { flag(e.offsetX, e.offsetY); }
     }
 }
